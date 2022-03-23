7b550e50a5dd3b7a053ed8ed1f785424cc0dbb5d
Miljan Radovic <miljan.radovic@gmail.com>
2022-03-23 16:23:34 +0400

    Update cron_expander.sql

--- cron_expander.sql
+++ cron_expander.sql
@@ -1,3 +1,5 @@
+
+
 -- =============================================
 -- Author:      Miljan Radovic
 -- Create date: 2022-03-22
@@ -23,7 +25,39 @@
 	union all
 	select '5 0/11 * * 2/2' as cron
 	union all
-	select '11-22,15/10 0/11 7/2 2 0/3' as cron
+	select '5 0 * 8 *' as cron
+	union all
+	select '15 14 1 * *' as cron
+	union all
+	select '0 22 * * 1-5' as cron
+	union all
+	select '23 0-20/2 * * *' as cron
+	union all
+	select '0 0,12 1 */2 *' as cron
+	union all
+	select '0 4 8-14 * *' as cron
+	union all
+	select '0 0,12 1 */2 *' as cron
+	union all
+	select '0 0 1,15 * 3' as cron
+	union all
+	select '11 0/2 * * 1-5' as cron
+	union all
+	select '' as cron
+	union all
+	select '0/15 0/3 * * 1-6' as cron
+	union all
+	select '10 0/4 1 * *' as cron
+	union all
+	select '' as cron
+	union all
+	select '1-10,15/5 6-14 1-10 3-10 0/3' as cron
+	union all
+	select '14 6 * 1/3 1-5' as cron
+	union all
+	select '* * * * *' as cron
+	union all
+	select '13 13 1/3 * *' as cron
 )
 , lv0 AS (SELECT 0 g UNION ALL SELECT 0)
 , lv1 AS (SELECT 0 g FROM lv0 a CROSS JOIN lv0 b) -- 4
@@ -48,6 +82,11 @@
 	select top 7 5 as idx, 'day_of_week', n-1
 	from Tally
 )
+, cron_parts_ranges as (
+	select idx, part_name, min([value]) as min_value, max([value]) as max_value
+	from cron_parts
+	group by idx, part_name
+)
 , expand_parts as (
 	select c.*, p.value, cast(row_number() over (partition by c.cron order by (select null)) as smallint) as idx
 	from crons as c
@@ -58,22 +97,15 @@
 	from expand_parts as p
 	cross apply string_split(p.value, ',') as v
 )
--- Calculate ranges and steps, if any in the cron
-, get_ranges_and_steps as (
-	select 
+, expand_steps as (
+		select 
 		c.idx,
 		c.cron,
 		c.value as cron_part,
 		case
-			when c.value like '%-%' then substring(c.value, 0, charindex('-', c.value))
 			when c.value like '%/%' then substring(c.value, 0, charindex('/', c.value))
 			else c.value
 		end as [value],
-		cast(
-			case
-				when c.value like '%-%' then substring(c.value, charindex('-', c.value) + 1, 1000)
-			end as smallint)
-		as [range],
 		cast(
 			case
 				when c.value like '%/%' then substring(c.value, charindex('/', c.value) + 1, 1000)
@@ -81,28 +113,44 @@
 		as [step]
 	from expand_commas as c
 )
--- Expand 
+
+-- Calculate ranges and steps, if any in the cron
+, expand_ranges as (
+	select 
+		c.idx,
+		c.cron,
+		c.value as cron_part,
+		case
+			when c.value like '%-%' then substring(c.value, 0, charindex('-', c.value))
+			else c.value
+		end as [value],
+		cast(
+			case
+				when c.value like '%-%' then substring(c.value, charindex('-', c.value) + 1, 1000)
+			end
+			as smallint)
+		as [range],
+		[step]
+	from expand_steps as c
+)
+--select * from expand_ranges
 , expand_all as (
-	select distinct rs.idx, rs.cron, rs.cron_part, cp.part_name, cast(cp.value as smallint) as [value]
-	from get_ranges_and_steps as rs
+	select rs.idx, rs.cron, rs.cron_part, cp.part_name, cast(cp.value as smallint) as [value]
+	from expand_ranges as rs
+	inner join cron_parts_ranges cr
+		on rs.idx = cr.idx
 	inner join cron_parts as cp
 		on rs.idx = cp.idx
-		and (
-			(
-				rs.[step] is not null 
-				and (cp.[value] - cast(replace(rs.value, '*', cp.value) as smallint)) % rs.[step]  = 0
-				and cp.[value] >= cast(replace(rs.value, '*', cp.value) as smallint)
-			)
-			or
-			(
-				rs.[range] is not null and cp.[value] between cast(replace(rs.value, '*', cp.value) as smallint) and cast(rs.[range] as smallint)
-			)
-			or
-			(
-				rs.[step] is null and rs.[range] is null and replace(rs.value, '*', cp.value) = cp.value
-			)
-		)
+		-- no range
+		and cp.value = case when rs.[range] is null then replace(rs.value, '*', cp.value) else cp.value end
+		-- step
+		and cr.min_value = case when rs.step is not null then cp.[value] % rs.[step] else cr.min_value end
+		and cp.[value] >= case when rs.step is not null then cast(replace(rs.value, '*', cp.value) as smallint) else cp.value end
+		-- range
+		and cp.[value] >= case when rs.[range] is not null then cast(replace(rs.value, '*', cp.value) as smallint) else cp.value end
+		and cp.[value] >= case when rs.[range] is not null then cast(rs.[range] as smallint) else cp.value end
 )
+
 -- Dummy up years to use in datetime
 , years as (
 	select year(getdate()) - 1 as [year]
@@ -147,7 +195,7 @@
 	group by cron
 )
 -- Pivot rows into column to get prev and next as columns, not column values
-, pivotted as (
+--, pivotted as (
 	SELECT cron, prev, next
 	FROM  
 	(
@@ -158,12 +206,13 @@
 	  max(datetime)  
 	  FOR type IN ([prev], [next])  
 	) AS PivotTable
-)
--- Comment code below if you are not interested in second next value
+--)
+/*
+-- Uncomment code below and cte above if you are interested in second next value
 select p.cron, p.prev, p.[next], min([datetime]) as [then]
 from pivotted as p
 inner join expand_dates as e
 	on p.cron = e.cron
 	and e.[datetime] > p.[next]
 group by p.cron, p.prev, p.[next]
-	
+	*/
